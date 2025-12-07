c3f6b50cc621f509ede6f0a133df35e8fc0c1887
Samur <107584285+Samur000@users.noreply.github.com>
2025-12-07 16:43:54 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const base = '/SDVIG/';
+const base = '/SDVIG-TEST/';
 
 export default defineConfig({
 	base,
